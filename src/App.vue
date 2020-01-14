3d954049a98c572211ebb95aec96712d8e9d5cc8
Jeremy zhao <zhaoyimeng@foxmail.com>
2020-01-14 20:33:10 +0800

    新增login功能

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
+  <!-- 这个 id=app 只是跟下面的 css 对应 -->
   <div id="app">
     <img src="./assets/logo.png">
+    <!-- outer-view 是一个容器，名字叫“路由视图”，意思是当前路由（ URL）指向的内容将显示在这个容器中。
+    也就是说，其它的组件即使拥有自己的路由（URL，需要在 router 文件夹的 index.js 文件里定义），也只不
+    过表面上是一个单独的页面，实际上只是在根组件 App.vue 中-->
     <router-view/>
   </div>
 </template>
 
+<!-- export default 是 ES6 的语法，意思是将这个组件整体导出，之后就可以使用 import 导入组件了 -->
 <script>
 export default {
   name: 'App'
--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 设置反向代理，前端请求默认发送到 http://localhost:8443/api
+var axios = require('axios')
+axios.default.baseURI = 'http://localhost:8443/api'
+
+// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
+Vue.prototype.$axios = axios
+
+// 作用是阻止vue 在启动时生成生产提示
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
+  // el 属性提供一个在页面上已存在的 DOM 元素作为 Vue 对象的挂载目标
   el: '#app',
+  // router 代表该对象包含 Vue Router，并使用项目中定义的路由
   router,
-  components: { App },
+  // components 表示该对象包含的 Vue 组件
+  components: {App},
+  // template 是用一个字符串模板作为 Vue 实例的标识使用
   template: '<App/>'
 })
