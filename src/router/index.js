b28e87c93be5c32f9ab07ae4adedc792de530229
Jeremy zhao <zhaoyimeng@foxmail.com>
2020-01-14 21:15:00 +0800

    优化login代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import AppIndex from '@/components/home/Appindex.vue'
 import Login from '@/components/Login.vue'
 
 Vue.use(Router)
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex
     }
   ]
 })
